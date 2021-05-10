8b3381d728f04d0a7fd7ca6a30ab5c79538aa9a4
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 05:59:37 +0430

    Add ProgressBarIndexViewModel.cs

---
+++ AMZEnterpriseWebsite/Areas/Panel/Models/ViewModels/ProgressBarIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using System.Collections.Generic;
+
+namespace AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels
+{
+    public class ProgressBarIndexViewModel : IEntity
+    {
+        public int Id { get; set; }
+        public IList<ProgressBarFormViewModel> ProgressBarFormViewModels { get; set; }
+    }
+}
