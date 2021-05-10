ab8425070bb046ca7b5d9ac236ed6756183a64e3
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 05:57:18 +0430

    Add PostCategoryFormViewModel.cs

---
+++ AMZEnterpriseWebsite/Areas/Panel/Models/ViewModels/PostCategoryFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Models.Constants;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels
+{
+    public class PostCategoryFormViewModel : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = ConstantValidationErrorMessages.RequiredMsg)]
+        [MaxLength(256, ErrorMessage = ConstantValidationErrorMessages.MaxLengthMsg)]
+        public string Title { get; set; }
+        [Display(Name = "تاریخ ایجاد")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public DateTime LastEditDate { get; set; }
+    }
+}
