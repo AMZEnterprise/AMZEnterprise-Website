c6748eabd64dd5777268a6867526fd2c9dfb0f15
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:42:32 +0430

    Add post comments repository

---
+++ AMZEnterpriseWebsite/Persistence/EfCoreRepositories/PostCommentRepository.cs
@@ -0,0 +1,145 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Data;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.EfCoreRepositories
+{
+    public class PostCommentRepository : IPostCommentRepository
+    {
+        private readonly IApplicationDbContext _context;
+
+        public PostCommentRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<PostComment> GetAll()
+        {
+            return _context.PostComments
+                .Include(x => x.Children)
+                .Include(x => x.User)
+                .Include(x => x.Post)
+                .AsQueryable();
+        }
+
+        public async Task<IEnumerable<PostComment>> GetAllByPostId(int postId)
+        {
+            var postComments = await GetAll()
+                .Where(x => (x.PostId == postId && x.PostCommentStatus == PostCommentStatus.Accepted))
+                .ToListAsync();
+
+            foreach (var comment in postComments)
+            {
+                if (comment.ParentId != null && !postComments.Exists(x => x.Id == comment.ParentId))
+                {
+                    comment.ParentId = null;
+                }
+            }
+
+            return postComments;
+        }
+
+        public async Task<PostComment> GetById(int id)
+        {
+            return await _context.PostComments
+                .Include(x => x.User)
+                .Include(x => x.Post)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public void Insert(PostComment postComment)
+        {
+            if (postComment.UserId == null)
+            {
+                if (postComment.UserFullName == null)
+                    throw new NullReferenceException(nameof(PostComment.UserFullName));
+
+                if (postComment.Email == null)
+                    throw new NullReferenceException(nameof(PostComment.Email));
+            }
+
+            if (postComment.Body == null)
+                throw new NullReferenceException(nameof(PostComment.Body));
+
+            if (postComment.Ip == null)
+                throw new NullReferenceException(nameof(PostComment.Ip));
+
+
+            postComment.IsEdited = false;
+            postComment.CreateDate = postComment.LastEditDate = DateTime.Now;
+
+            _context.PostComments.Add(postComment);
+        }
+
+        public void Update(PostComment postComment)
+        {
+            if (postComment.UserId == null)
+            {
+                if (postComment.UserFullName == null)
+                    throw new NullReferenceException(nameof(PostComment.UserFullName));
+
+                if (postComment.Email == null)
+                    throw new NullReferenceException(nameof(PostComment.Email));
+            }
+
+            if (postComment.Body == null)
+                throw new NullReferenceException(nameof(PostComment.Body));
+
+            if (postComment.Ip == null)
+                throw new NullReferenceException(nameof(PostComment.Ip));
+
+            postComment.IsEdited = true;
+            postComment.LastEditDate = DateTime.Now;
+
+            _context.PostComments.Update(postComment);
+        }
+
+        public async Task UpdateChildrenStatus(int id, PostCommentStatus status)
+        {
+            if (status == PostCommentStatus.Rejected || status == PostCommentStatus.Unclear)
+            {
+                var children = GetAll().Where(x => x.ParentId == id).ToList();
+
+                foreach (var child in children)
+                {
+                    await UpdateChildrenStatus(child.Id, status);
+                    var currentChild = await GetById(child.Id);
+
+                    currentChild.PostCommentStatus = status;
+                    currentChild.IsEdited = true;
+                    currentChild.LastEditDate = DateTime.Now;
+
+                    Update(currentChild);
+                }
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            var postComment = await GetById(id);
+            if (postComment != null)
+                _context.PostComments.Remove(postComment);
+        }
+
+        public async Task DeleteChildren(int id)
+        {
+            var children = GetAll().Where(x => x.ParentId == id).ToList();
+
+            foreach (var child in children)
+            {
+                await DeleteChildren(child.Id);
+                await Delete(child.Id);
+            }
+        }
+
+        public async Task<int> Count()
+        {
+            return await _context.PostComments.CountAsync();
+        }
+    }
+}
---
+++ AMZEnterpriseWebsite/Persistence/Repositories/IPostCommentRepository.cs
@@ -0,0 +1,65 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.Repositories
+{
+    /// <summary>
+    /// Represents post comments repository interface 
+    /// </summary>
+    public interface IPostCommentRepository
+    {
+        /// <summary>
+        /// Get all post comments
+        /// </summary>
+        /// <returns>returns all post comments</returns>
+        IQueryable<PostComment> GetAll();
+        /// <summary>
+        /// Get a post all comments
+        /// </summary>
+        /// <param name="postId">post id</param>
+        /// <returns>return an IEnumerable of the post comments</returns>
+        Task<IEnumerable<PostComment>> GetAllByPostId(int postId);
+        /// <summary>
+        /// Get a post comment by id
+        /// </summary>
+        /// <param name="id">post comment id</param>
+        /// <returns>returns a specific post comment or null</returns>
+        Task<PostComment> GetById(int id);
+        /// <summary>
+        /// Insert new post comment
+        /// </summary>
+        /// <param name="postComment">new post comment</param>
+        void Insert(PostComment postComment);
+        /// <summary>
+        /// Update existing post comment
+        /// </summary>
+        /// <param name="postComment">updated post comment</param>
+        void Update(PostComment postComment);
+        /// <summary>
+        /// Update a post comment all children status
+        /// </summary>
+        /// <param name="id">post comment id</param>
+        /// <param name="postCommentStatus">updated post comment status</param>
+        /// <returns></returns>
+        Task UpdateChildrenStatus(int id, PostCommentStatus postCommentStatus);
+        /// <summary>
+        /// Delete a post comment by id
+        /// </summary>
+        /// <param name="id">post comment id</param>
+        /// <returns></returns>
+        Task Delete(int id);
+        /// <summary>
+        /// Delete a post comment all children
+        /// </summary>
+        /// <param name="id">post comment id</param>
+        /// <returns></returns>
+        Task DeleteChildren(int id);
+        /// <summary>
+        /// Count total post comments
+        /// </summary>
+        /// <returns>returns post comments count</returns>
+        Task<int> Count();
+    }
+}
