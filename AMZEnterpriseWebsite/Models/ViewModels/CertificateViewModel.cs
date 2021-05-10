8bcea07a20706e4b2522637bb5c3f401b0932324
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:18:30 +0430

    Add CertificateViewModel.cs

---
+++ AMZEnterpriseWebsite/Models/ViewModels/CertificateViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AMZEnterpriseWebsite.Models.ViewModels
+{
+    public class CertificateViewModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string FilePath { get; set; }
+    }
+}
