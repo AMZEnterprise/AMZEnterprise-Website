846f8bf1b62057ee19d2936b2bbbce72668792da
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:51:03 +0430

    Add progress bar repository

---
+++ AMZEnterpriseWebsite/Persistence/EfCoreRepositories/ProgressBarRepository.cs
@@ -0,0 +1,61 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Data;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.EfCoreRepositories
+{
+    public class ProgressBarRepository : IProgressBarRepository
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ProgressBarRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<ProgressBar> GetAll()
+        {
+            return _context.ProgressBars.AsQueryable();
+        }
+
+        public async Task<ProgressBar> GetById(int id)
+        {
+            return await _context.ProgressBars.FindAsync(id);
+        }
+
+        public async Task Insert(ProgressBar progressBar)
+        {
+            var maxSort = await _context.ProgressBars
+                .MaxAsync(x => (int?)x.SortIndex) ?? 0;
+
+            progressBar.SortIndex = (maxSort == 0 && await Count() == 0) ? 0 : maxSort + 1;
+
+            _context.ProgressBars.Add(progressBar);
+        }
+
+
+        public void UpdateAll(IEnumerable<ProgressBar> progressBars)
+        {
+            _context.ProgressBars.UpdateRange(progressBars);
+        }
+
+        public async Task Delete(int id)
+        {
+            var progressBar = await GetById(id);
+
+            if (progressBar != null)
+            {
+                _context.ProgressBars.Remove(progressBar);
+            }
+        }
+
+        public async Task<int> Count()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AMZEnterpriseWebsite/Persistence/Repositories/IProgressBarRepository.cs
@@ -0,0 +1,46 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.Repositories
+{
+    /// <summary>
+    /// Represents progress bar repository interface 
+    /// </summary>
+    public interface IProgressBarRepository
+    {
+        /// <summary>
+        /// Get all progressBars
+        /// </summary>
+        /// <returns>returns all progressBars</returns>
+        IQueryable<ProgressBar> GetAll();
+        /// <summary>
+        /// Get a progressBar by id
+        /// </summary>
+        /// <param name="id">progressBar id</param>
+        /// <returns>returns a specific progressBar or null</returns>
+        Task<ProgressBar> GetById(int id);
+        /// <summary>
+        /// Insert new progressBar
+        /// </summary>
+        /// <param name="progressBar">new progressBar</param>
+        Task Insert(ProgressBar progressBar);
+        /// <summary>
+        /// Update all existing progressBar
+        /// </summary>
+        /// <param name="progressBars">Updated progressBars as IEnumerable</param>
+        void UpdateAll(IEnumerable<ProgressBar> progressBars);
+        /// <summary>
+        /// Delete a progressBar by id
+        /// </summary>
+        /// <param name="id">progressBar id</param>
+        /// <returns></returns>
+        Task Delete(int id);
+        /// <summary>
+        /// Count total contacts
+        /// </summary>
+        /// <returns>returns contacts count</returns>
+        Task<int> Count();
+    }
+}
