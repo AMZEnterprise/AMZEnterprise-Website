0ab5b94d2524976d3f3547acc7f0fd0c9b6a2aaa
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:05:46 +0430

    Add SettingsController.cs

---
+++ AMZEnterpriseWebsite/Areas/Panel/Controllers/SettingsController.cs
@@ -0,0 +1,58 @@
+﻿using AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels;
+using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Models.Constants;
+using AMZEnterpriseWebsite.Persistence;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Areas.Panel.Controllers
+{
+    [Area(ConstantAreas.Panel)]
+    [Authorize(Roles = ConstantUserRoles.SuperAdmin)]
+    public class SettingsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public SettingsController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var setting = await _unitOfWork.SettingRepository.Get();
+
+            if (setting == null)
+            {
+                return NotFound();
+            }
+
+            return View(_mapper.Map<Setting, SettingFormViewModel>(setting));
+        }
+
+        [HttpPost, ActionName(nameof(Index))]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> IndexPost(int id, SettingFormViewModel settingFormViewModel)
+        {
+            if (id != settingFormViewModel.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var setting = _mapper.Map<SettingFormViewModel, Setting>(settingFormViewModel);
+                _unitOfWork.SettingRepository.Update(setting);
+                await _unitOfWork.Complete();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(settingFormViewModel);
+        }
+    }
+}
