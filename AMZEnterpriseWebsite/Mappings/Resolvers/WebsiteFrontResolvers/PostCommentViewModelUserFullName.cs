12cfefccba69123e53a9159ead4152406e14c4ae
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:09:02 +0430

    Add PostCommentViewModelUserFullName.cs

---
+++ AMZEnterpriseWebsite/Mappings/Resolvers/WebsiteFrontResolvers/PostCommentViewModelUserFullName.cs
@@ -0,0 +1,17 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Models.ViewModels;
+using AutoMapper;
+
+namespace AMZEnterpriseWebsite.Mappings.Resolvers.WebsiteFrontResolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostCommentViewModel UserFullName
+    /// </summary>
+    public class PostCommentViewModelUserFullName : IValueResolver<PostComment, PostCommentViewModel, string>
+    {
+        public string Resolve(PostComment source, PostCommentViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.User == null ? source.UserFullName : source.User.FirstName + " " + source.User.LastName;
+        }
+    }
+}
