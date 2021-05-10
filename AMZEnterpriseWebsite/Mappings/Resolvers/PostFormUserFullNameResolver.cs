3e269d1163fbe5090c7c073a612ec257382aadf8
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:15:11 +0430

    Add PostFormUserFullNameResolver.cs

---
+++ AMZEnterpriseWebsite/Mappings/Resolvers/PostFormUserFullNameResolver.cs
@@ -0,0 +1,17 @@
+﻿using AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels;
+using AMZEnterpriseWebsite.Core.Domain;
+using AutoMapper;
+
+namespace AMZEnterpriseWebsite.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostFormViewModel UserFullName
+    /// </summary>
+    public class PostFormUserFullNameResolver : IValueResolver<Post, PostFormViewModel, string>
+    {
+        public string Resolve(Post source, PostFormViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.User?.FirstName + " " + source.User?.LastName;
+        }
+    }
+}
