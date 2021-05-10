1cc70080270f3816a0f4182bd80083aa5f7c5d97
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:19:01 +0430

    Add DonateViewModel.cs

---
+++ AMZEnterpriseWebsite/Models/ViewModels/DonateViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace AMZEnterpriseWebsite.Models.ViewModels
+{
+    public class DonateViewModel
+    {
+        public string WalletName1 { get; set; }
+        public string WalletAddress1 { get; set; }
+        public string WalletName2 { get; set; }
+        public string WalletAddress2 { get; set; }
+        public string WalletName3 { get; set; }
+        public string WalletAddress3 { get; set; }
+        public string WalletName4 { get; set; }
+        public string WalletAddress4 { get; set; }
+        public string WalletName5 { get; set; }
+        public string WalletAddress5 { get; set; }
+        public string WalletName6 { get; set; }
+        public string WalletAddress6 { get; set; }
+        public string WalletName7 { get; set; }
+        public string WalletAddress7 { get; set; }
+        public string WalletName8 { get; set; }
+        public string WalletAddress8 { get; set; }
+        public string WalletName9 { get; set; }
+        public string WalletAddress9 { get; set; }
+        public string WalletName10 { get; set; }
+        public string WalletAddress10 { get; set; }
+    }
+}
