8ae5635b527ef72a73662abd593d1b41a1b7a501
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:21:10 +0430

    Add LatestPosts viewComponent

---
+++ AMZEnterpriseWebsite/ViewComponents/LatestPostsViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Models.ViewModels;
+using AMZEnterpriseWebsite.Persistence;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.ViewComponents
+{
+    public class LatestPostsViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public LatestPostsViewComponent(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _unitOfWork.PostRepository
+                .GetAll()
+                .OrderByDescending(x => x.CreateDate)
+                .Take(2)
+                .ToListAsync();
+
+            return View(_mapper.Map<IEnumerable<Post>, IEnumerable<PostViewModel>>(posts));
+        }
+    }
+}
