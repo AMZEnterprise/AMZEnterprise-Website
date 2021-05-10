1c8cdac910135ffaa51c4acb8575d3357197266f
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:14:29 +0430

    Add BlogSidebarViewModel.cs

---
+++ AMZEnterpriseWebsite/Models/ViewModels/BlogSidebarViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AMZEnterpriseWebsite.Models.ViewModels
+{
+    public class BlogSidebarViewModel
+    {
+        public IEnumerable<PostViewModel> PostViewModels { get; set; }
+        public IEnumerable<PostCategoryViewModel> PostCategoryViewModels { get; set; }
+        public IEnumerable<string> PostTags { get; set; }
+    }
+}
