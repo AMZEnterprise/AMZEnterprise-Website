1f0ada88e558cda58d39a81ca25a8f3c45600708
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:52:27 +0430

    Add UnitOfWork

---
+++ AMZEnterpriseWebsite/Persistence/IUnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using AMZEnterpriseWebsite.Persistence.Repositories;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence
+{
+    /// <summary>
+    /// Represents unit of work interface 
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Represents certificate repository interface
+        /// </summary>
+        ICertificateRepository CertificateRepository { get; set; }
+        /// <summary>
+        /// Represents contact repository interface
+        /// </summary>
+        IContactRepository ContactRepository { get; set; }
+        /// <summary>
+        /// Represents post category repository interface
+        /// </summary>
+        IPostCategoryRepository PostCategoryRepository { get; set; }
+        /// <summary>
+        /// Represents post comment category repository interface
+        /// </summary>
+        IPostCommentRepository PostCommentRepository { get; set; }
+        /// <summary>
+        /// Represents post repository interface
+        /// </summary>
+        IPostRepository PostRepository { get; set; }
+        /// <summary>
+        /// Represents progressBar repository interface
+        /// </summary>
+        IProgressBarRepository ProgressBarRepository { get; set; }
+        /// <summary>
+        /// Represents setting repository interface
+        /// </summary>
+        ISettingRepository SettingRepository { get; set; }
+        /// <summary>
+        /// Represents user repository interface
+        /// </summary>
+        IUserRepository UserRepository { get; set; }
+        /// <summary>
+        /// Commit changes to database
+        /// </summary>
+        /// <returns></returns>
+        Task Complete();
+    }
+}
---
+++ AMZEnterpriseWebsite/Persistence/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Data;
+using AMZEnterpriseWebsite.Persistence.EfCoreRepositories;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(
+            ApplicationDbContext context,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IPasswordHasher<User> passwordHasher
+        )
+        {
+            _context = context;
+
+            CertificateRepository = new CertificateRepository(_context);
+            ContactRepository = new ContactRepository(_context);
+            PostCategoryRepository = new PostCategoryRepository(_context);
+            PostCommentRepository = new PostCommentRepository(_context);
+            PostRepository = new PostRepository(_context);
+            ProgressBarRepository = new ProgressBarRepository(_context);
+            SettingRepository = new SettingRepository(_context);
+            UserRepository = new UserRepository(userManager, signInManager, passwordHasher);
+        }
+
+        public ICertificateRepository CertificateRepository { get; set; }
+        public IContactRepository ContactRepository { get; set; }
+        public IPostCategoryRepository PostCategoryRepository { get; set; }
+        public IPostCommentRepository PostCommentRepository { get; set; }
+        public IPostRepository PostRepository { get; set; }
+        public IProgressBarRepository ProgressBarRepository { get; set; }
+        public ISettingRepository SettingRepository { get; set; }
+        public IUserRepository UserRepository { get; set; }
+
+        public async Task Complete()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
