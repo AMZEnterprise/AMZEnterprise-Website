853cb753d3ea6e2ddcf88cbd12427e5ecf28d179
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:14:59 +0430

    Add PostCommentIndexViewModelUserFullNameResolver.cs

---
+++ AMZEnterpriseWebsite/Mappings/Resolvers/PostCommentIndexViewModelUserFullNameResolver.cs
@@ -0,0 +1,17 @@
+﻿using AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels;
+using AMZEnterpriseWebsite.Core.Domain;
+using AutoMapper;
+
+namespace AMZEnterpriseWebsite.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostCommentIndexViewModel UserFullName
+    /// </summary>
+    public class PostCommentIndexViewModelUserFullNameResolver : IValueResolver<PostComment, PostCommentIndexViewModel, string>
+    {
+        public string Resolve(PostComment source, PostCommentIndexViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.User == null ? source.UserFullName : source.User.FirstName + " " + source.User.LastName;
+        }
+    }
+}
