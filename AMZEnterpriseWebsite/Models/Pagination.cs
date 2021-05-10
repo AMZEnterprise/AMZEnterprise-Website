2455303eac9bee492495cfa5a51d38b5659d8788
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 05:45:33 +0430

    Add Pagination.cs

---
+++ AMZEnterpriseWebsite/Models/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AMZEnterpriseWebsite.Models
+{
+    public class Pagination
+    {
+        public Pager Pager { get; set; }
+
+        /// <summary>
+        /// Query string parameters dictionary
+        /// </summary>
+        public Dictionary<string, string> Params { get; set; }
+    }
+}
