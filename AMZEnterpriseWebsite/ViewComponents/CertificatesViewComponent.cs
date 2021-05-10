9d682b248e3fbd15cfc35a2846ac79ac259e2411
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:19:12 +0430

    Add Certificates viewComponent

---
+++ AMZEnterpriseWebsite/ViewComponents/CertificatesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Models.ViewModels;
+using AMZEnterpriseWebsite.Persistence;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.ViewComponents
+{
+    public class CertificatesViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CertificatesViewComponent(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var certificates =
+                await _unitOfWork.CertificateRepository.GetAll().ToListAsync();
+
+            return View(_mapper.Map<IEnumerable<Certificate>, IEnumerable<CertificateViewModel>>(certificates));
+        }
+    }
+}
