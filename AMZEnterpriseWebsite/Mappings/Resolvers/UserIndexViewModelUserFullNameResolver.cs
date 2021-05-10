95d7a67b1d8cf1edfc72bd4fb5e275488c4174dc
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:17:30 +0430

    Add UserIndexViewModelUserFullNameResolver.cs

---
+++ AMZEnterpriseWebsite/Mappings/Resolvers/UserIndexViewModelUserFullNameResolver.cs
@@ -0,0 +1,17 @@
+﻿using AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels;
+using AMZEnterpriseWebsite.Core.Domain;
+using AutoMapper;
+
+namespace AMZEnterpriseWebsite.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for UserIndexViewModel UserFullName
+    /// </summary>
+    public class UserIndexViewModelUserFullNameResolver : IValueResolver<User, UserIndexViewModel, string>
+    {
+        public string Resolve(User source, UserIndexViewModel destination, string destMember, ResolutionContext context)
+        {
+            return source.FirstName + " " + source.LastName;
+        }
+    }
+}
