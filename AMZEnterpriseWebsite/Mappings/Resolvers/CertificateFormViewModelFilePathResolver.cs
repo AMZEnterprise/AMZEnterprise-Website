483ff7ee34ca5b19123b7f13c04655fe1c17fa9a
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:12:35 +0430

    Add CertificateFormViewModelFilePathResolver.cs

---
+++ AMZEnterpriseWebsite/Mappings/Resolvers/CertificateFormViewModelFilePathResolver.cs
@@ -0,0 +1,31 @@
+﻿using AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels;
+using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Services.FileHandler;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AMZEnterpriseWebsite.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for CertificateFormViewModel FilePath
+    /// </summary>
+    public class CertificateFormViewModelFilePathResolver : IValueResolver<Certificate, CertificateFormViewModel, string>
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IFileHandler _fileHandler;
+
+        public CertificateFormViewModelFilePathResolver(IWebHostEnvironment webHostEnvironment, IFileHandler fileHandler)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _fileHandler = fileHandler;
+        }
+
+        public string Resolve(Certificate source, CertificateFormViewModel destination, string destMember, ResolutionContext context)
+        {
+            return _fileHandler.GetFileSource(
+                _webHostEnvironment.WebRootPath,
+                source.FilesPathGuid.ToString(),
+                FileHandlerFolder.Certificates);
+        }
+    }
+}
