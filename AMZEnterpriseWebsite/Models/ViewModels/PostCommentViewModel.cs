5487f7be6d5ea8c03f9a12bdd440a699e3719d8e
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:30:18 +0430

    Add PostCommentViewModel.cs

---
+++ AMZEnterpriseWebsite/Models/ViewModels/PostCommentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AMZEnterpriseWebsite.Core.Domain;
+
+namespace AMZEnterpriseWebsite.Models.ViewModels
+{
+    public class PostCommentViewModel : IEntity
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool IsEdited { get; set; }
+        public string UserFullName { get; set; }
+    }
+}
