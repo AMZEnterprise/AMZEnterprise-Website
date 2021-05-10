126a26cd00fa963deaeccd7e04768f6f5be591b9
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 05:57:29 +0430

    Add PostCategoryIndexViewModel.cs

---
+++ AMZEnterpriseWebsite/Areas/Panel/Models/ViewModels/PostCategoryIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels
+{
+    public class PostCategoryIndexViewModel : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "عنوان")]
+        public string Title { get; set; }
+        [Display(Name = "تاریخ ایجاد")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public DateTime LastEditDate { get; set; }
+    }
+}
