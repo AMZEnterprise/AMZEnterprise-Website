50015e94657373cb1c35dbba2bcbcf3984d67130
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:06:42 +0430

    Add HomeController.cs

---
+++ AMZEnterpriseWebsite/Areas/Panel/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using AMZEnterpriseWebsite.Models.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMZEnterpriseWebsite.Areas.Panel.Controllers
+{
+    [Area(ConstantAreas.Panel)]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
