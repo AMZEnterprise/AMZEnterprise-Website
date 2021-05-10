3fbde2547d97871e48869f98bc25b86d55c88d15
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:40:20 +0430

    Add certificate repository

---
+++ AMZEnterpriseWebsite/Persistence/EfCoreRepositories/CertificateRepository.cs
@@ -0,0 +1,57 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Data;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.EfCoreRepositories
+{
+    public class CertificateRepository : ICertificateRepository
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CertificateRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Certificate> GetAll()
+        {
+            return _context.Certificates.AsQueryable();
+        }
+
+        public async Task<Certificate> GetById(int id)
+        {
+            return await _context.Certificates.FindAsync(id);
+        }
+
+        public void Insert(Certificate certificate)
+        {
+            certificate.CreateDate = certificate.LastEditDate = DateTime.Now;
+            _context.Certificates.Add(certificate);
+        }
+
+        public void Update(Certificate certificate)
+        {
+            certificate.LastEditDate = DateTime.Now;
+            _context.Certificates.Update(certificate);
+        }
+
+        public async Task Delete(int id)
+        {
+            var certificate = await GetById(id);
+
+            if (certificate != null)
+            {
+                _context.Certificates.Remove(certificate);
+            }
+        }
+
+        public async Task<int> Count()
+        {
+            return await _context.Certificates.CountAsync();
+        }
+    }
+}
---
+++ AMZEnterpriseWebsite/Persistence/Repositories/ICertificateRepository.cs
@@ -0,0 +1,46 @@
+﻿using AMZEnterpriseWebsite.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using AMZEnterpriseWebsite.Core.Domain;
+
+namespace AMZEnterpriseWebsite.Persistence.Repositories
+{
+    /// <summary>
+    /// Represents certificate repository interface 
+    /// </summary>
+    public interface ICertificateRepository
+    {
+        /// <summary>
+        /// Get all certificates
+        /// </summary>
+        /// <returns>returns all certificates</returns>
+        IQueryable<Certificate> GetAll();
+        /// <summary>
+        /// Get a certificate by id
+        /// </summary>
+        /// <param name="id">certificate id</param>
+        /// <returns>returns a specific certificate or null</returns>
+        Task<Certificate> GetById(int id);
+        /// <summary>
+        /// Insert new certificate
+        /// </summary>
+        /// <param name="certificate">new certificate</param>
+        void Insert(Certificate certificate);
+        /// <summary>
+        /// Update existing certificate
+        /// </summary>
+        /// <param name="certificate">updated certificate</param>
+        void Update(Certificate certificate);
+        /// <summary>
+        /// Delete a certificate by id
+        /// </summary>
+        /// <param name="id">certificate id</param>
+        /// <returns></returns>
+        Task Delete(int id);
+        /// <summary>
+        /// Count total certificates
+        /// </summary>
+        /// <returns>returns certificates count</returns>
+        Task<int> Count();
+    }
+}
