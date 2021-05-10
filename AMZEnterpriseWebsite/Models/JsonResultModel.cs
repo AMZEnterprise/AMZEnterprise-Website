02dbca8452a29263843520d7a789e0811a6e48d3
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 05:45:10 +0430

    Add JsonResultModel.cs

---
+++ AMZEnterpriseWebsite/Models/JsonResultModel.cs
@@ -0,0 +1,24 @@
+﻿namespace AMZEnterpriseWebsite.Models
+{
+    /// <summary>
+    /// Json result model
+    /// </summary>
+    public class JsonResultModel
+    {
+        public JsonResultStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+
+    /// <summary>
+    /// Json result status code
+    /// </summary>
+    public enum JsonResultStatusCode
+    {
+        Success = 200,
+        Unauthorized = 401,
+        NotFound = 404,
+        ModelStateIsNotValid = 422,
+        ServerError = 500
+    }
+}
