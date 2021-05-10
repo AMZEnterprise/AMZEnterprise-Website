f17bd872569630f0064b87a416b92203a2dbb29e
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:51:44 +0430

    Add setting repository

---
+++ AMZEnterpriseWebsite/Persistence/EfCoreRepositories/SettingRepository.cs
@@ -0,0 +1,27 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Data;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.EfCoreRepositories
+{
+    public class SettingRepository : ISettingRepository
+    {
+        private readonly IApplicationDbContext _context;
+        public SettingRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Setting> Get()
+        {
+            return await _context.Settings.FirstOrDefaultAsync();
+        }
+
+        public void Update(Setting setting)
+        {
+            _context.Settings.Update(setting);
+        }
+    }
+}
---
+++ AMZEnterpriseWebsite/Persistence/Repositories/ISettingRepository.cs
@@ -0,0 +1,22 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.Repositories
+{
+    /// <summary>
+    /// Represents setting repository interface 
+    /// </summary>
+    public interface ISettingRepository
+    {
+        /// <summary>
+        /// Get setting
+        /// </summary>
+        /// <returns>returns setting</returns>
+        Task<Setting> Get();
+        /// <summary>
+        /// Update setting
+        /// </summary>
+        /// <param name="setting">updated setting</param>
+        void Update(Setting setting);
+    }
+}
