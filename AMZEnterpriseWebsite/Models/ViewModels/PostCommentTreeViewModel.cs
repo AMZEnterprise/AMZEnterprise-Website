8d572d026bf12da22a53b9acf1f43ac78ad2d6ad
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:31:40 +0430

    Add PostCommentTreeViewModel.cs

---
+++ AMZEnterpriseWebsite/Models/ViewModels/PostCommentTreeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AMZEnterpriseWebsite.Models.ViewModels
+{
+    public class PostCommentTreeViewModel
+    {
+        public int? CommentSeed { get; set; }
+        public IEnumerable<PostCommentViewModel> PostCommentViewModels { get; set; }
+    }
+}
