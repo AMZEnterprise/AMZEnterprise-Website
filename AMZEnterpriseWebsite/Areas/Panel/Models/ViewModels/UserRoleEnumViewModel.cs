832b5e901255efad2f33a3d224bb619d678cc22a
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:01:19 +0430

    Add UserRoleEnumViewModel.cs

---
+++ AMZEnterpriseWebsite/Areas/Panel/Models/ViewModels/UserRoleEnumViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels
+{
+    public enum UserRoleEnumViewModel
+    {
+        [Display(Name = "مدیر کل", Order = 0)]
+        SuperAdmin,
+        [Display(Name = "مدیر", Order = 1)]
+        Admin,
+        [Display(Name = "نویسنده", Order = 2)]
+        Writer,
+    }
+}
