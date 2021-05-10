c6168f438d2b944069d1a2253a638e2b40f1124d
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:25:24 +0430

    Add PostCategoryViewModel.cs

---
+++ AMZEnterpriseWebsite/Models/ViewModels/PostCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+
+namespace AMZEnterpriseWebsite.Models.ViewModels
+{
+    public class PostCategoryViewModel : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PostsCount { get; set; }
+    }
+}
