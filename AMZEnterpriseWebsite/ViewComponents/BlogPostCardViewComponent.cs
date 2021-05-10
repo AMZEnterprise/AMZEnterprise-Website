add5cbddb9125cc624e95d9129e7aacbc429e8aa
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:18:38 +0430

    Refactor BlogPostCard viewComponent

--- AMZEnterpriseWebsite/ViewComponents/BlogPostCardViewComponent.cs
+++ AMZEnterpriseWebsite/ViewComponents/BlogPostCardViewComponent.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AMZEnterpriseWebsite.Data;
-using AMZEnterpriseWebsite.Models;
-using AMZEnterpriseWebsite.Models.ViewModels;
+﻿using AMZEnterpriseWebsite.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AMZEnterpriseWebsite.ViewComponents
 {
     public class BlogPostCardViewComponent : ViewComponent
     {
-        private readonly ApplicationDbContext _context;
-        public BlogPostCardViewComponent(ApplicationDbContext context)
+        public async Task<IViewComponentResult> InvokeAsync(PostViewModel postViewModel)
         {
-            _context = context;
-        }
-
-        public async Task<IViewComponentResult> InvokeAsync(Post blogPost)
-        {
-            return View(blogPost);
+            return View(postViewModel);
         }
     }
 }
