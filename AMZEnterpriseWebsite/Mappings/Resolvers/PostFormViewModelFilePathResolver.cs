9e9187e1f86c1ffdaeb15179064f109a32df91ab
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:15:22 +0430

    Add PostFormViewModelFilePathResolver.cs

---
+++ AMZEnterpriseWebsite/Mappings/Resolvers/PostFormViewModelFilePathResolver.cs
@@ -0,0 +1,31 @@
+﻿using AMZEnterpriseWebsite.Areas.Panel.Models.ViewModels;
+using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Services.FileHandler;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AMZEnterpriseWebsite.Mappings.Resolvers
+{
+    /// <summary>
+    /// Auto mapper resolver for PostFormViewModel FilePath
+    /// </summary>
+    public class PostFormViewModelFilePathResolver : IValueResolver<Post, PostFormViewModel, string>
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IFileHandler _fileHandler;
+
+        public PostFormViewModelFilePathResolver(IWebHostEnvironment webHostEnvironment, IFileHandler fileHandler)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _fileHandler = fileHandler;
+        }
+
+        public string Resolve(Post source, PostFormViewModel destination, string destMember, ResolutionContext context)
+        {
+            return _fileHandler.GetFileSource(
+                _webHostEnvironment.WebRootPath,
+                source.FilesPathGuid.ToString(),
+                FileHandlerFolder.Posts);
+        }
+    }
+}
