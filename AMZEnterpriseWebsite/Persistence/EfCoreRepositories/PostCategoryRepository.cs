32c76d780029ade68af58687f231a7dc82065c5c
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:42:03 +0430

    Add category repository

---
+++ AMZEnterpriseWebsite/Persistence/EfCoreRepositories/PostCategoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Data;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.EfCoreRepositories
+{
+    public class PostCategoryRepository : IPostCategoryRepository
+    {
+        private readonly IApplicationDbContext _context;
+
+        public PostCategoryRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<PostCategory> GetAll()
+        {
+            return _context.PostCategories.AsQueryable();
+        }
+
+        public async Task<PostCategory> GetById(int id)
+        {
+            return await _context.PostCategories.FindAsync(id);
+        }
+
+        public void Insert(PostCategory postCategory)
+        {
+            if (postCategory.Title == null)
+                throw new NullReferenceException(nameof(PostCategory.Title));
+
+            postCategory.CreateDate = postCategory.LastEditDate = DateTime.Now;
+
+            _context.PostCategories.Add(postCategory);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            if (postCategory.Title == null)
+                throw new NullReferenceException(nameof(PostCategory.Title));
+
+            postCategory.LastEditDate = DateTime.Now;
+
+            _context.PostCategories.Update(postCategory);
+        }
+
+        public async Task Delete(int id)
+        {
+            var postCategory = await GetById(id);
+            if (postCategory != null)
+                _context.PostCategories.Remove(postCategory);
+        }
+
+        public async Task<bool> IsPostCategoryExists(PostCategory postCategory)
+        {
+            return postCategory.Id == 0
+                ? await _context.PostCategories
+                    .AnyAsync(x => x.Title == postCategory.Title)
+                : await _context.PostCategories
+                    .AnyAsync(x => x.Title == postCategory.Title && x.Id == postCategory.Id);
+        }
+
+        public async Task<int> Count()
+        {
+            return await _context.PostCategories.CountAsync();
+        }
+    }
+}
---
+++ AMZEnterpriseWebsite/Persistence/Repositories/IPostCategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.Repositories
+{
+    /// <summary>
+    /// Represents post category repository interface 
+    /// </summary>
+    public interface IPostCategoryRepository
+    {
+        /// <summary>
+        /// Get all post categories
+        /// </summary>
+        /// <returns>returns all post categories</returns>
+        IQueryable<PostCategory> GetAll();
+        /// <summary>
+        /// Get a post category by id
+        /// </summary>
+        /// <param name="id">post category id</param>
+        /// <returns>returns a specific post category or null</returns>
+        Task<PostCategory> GetById(int id);
+        /// <summary>
+        /// Insert new post category
+        /// </summary>
+        /// <param name="postCategory">new post category</param>
+        void Insert(PostCategory postCategory);
+        /// <summary>
+        /// Update existing post category
+        /// </summary>
+        /// <param name="postCategory">updated post category</param>
+        void Update(PostCategory postCategory);
+        /// <summary>
+        /// Delete a post category by id
+        /// </summary>
+        /// <param name="id">post category id</param>
+        /// <returns></returns>
+        Task Delete(int id);
+        /// <summary>
+        /// Check whether post category currently exists
+        /// </summary>
+        /// <param name="postCategory">post category</param>
+        /// <returns>true if exists, false if not</returns>
+        Task<bool> IsPostCategoryExists(PostCategory postCategory);
+        /// <summary>
+        /// Count total post categories
+        /// </summary>
+        /// <returns>returns post categories count</returns>
+        Task<int> Count();
+    }
+}
