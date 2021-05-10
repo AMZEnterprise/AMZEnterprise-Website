30c30b599383064c3f10a8f86434bf5f6d7bfd3e
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 06:52:09 +0430

    Add user repository

---
+++ AMZEnterpriseWebsite/Persistence/EfCoreRepositories/UserRepository.cs
@@ -0,0 +1,174 @@
+﻿using AMZEnterpriseWebsite.Core.Domain;
+using AMZEnterpriseWebsite.Persistence.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.Persistence.EfCoreRepositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public UserRepository(
+            UserManager<User> userManager,
+            SignInManager<User> singInManager,
+            IPasswordHasher<User> passwordHasher)
+        {
+            _userManager = userManager;
+            _signInManager = singInManager;
+            _passwordHasher = passwordHasher;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return _userManager.Users.AsQueryable();
+        }
+
+        public async Task<User> GetByClaimsPrincipal(ClaimsPrincipal principal)
+        {
+            return await _userManager.GetUserAsync(principal);
+        }
+
+        public async Task<bool> IsInRole(User user, string role)
+        {
+            return await _userManager.IsInRoleAsync(user, role);
+        }
+
+        public async Task<User> FindByUserName(string userName)
+        {
+            return await _userManager.FindByNameAsync(userName);
+        }
+
+        public async Task SignOut()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task SignIn(User user, bool isPersistent)
+        {
+            await _signInManager.SignInAsync(user, isPersistent);
+        }
+
+        public async Task<SignInResult> SignInByPassword(User user, string password, bool isPersistent, bool lookoutOnFailure)
+        {
+            return await _signInManager.PasswordSignInAsync(user, password, isPersistent, lookoutOnFailure);
+        }
+
+        public async Task<string> GenerateChangePhoneNumberToken(User user, string phoneNumber)
+        {
+            return await _userManager.GenerateChangePhoneNumberTokenAsync(user, phoneNumber);
+        }
+
+        public async Task<bool> VerifyChangePhoneNumberToken(User user, string token, string phoneNumber)
+        {
+            return await _userManager.VerifyChangePhoneNumberTokenAsync(user, token, phoneNumber);
+        }
+
+        public async Task<IdentityResult> ChangePassword(User user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
+
+        public async Task<IdentityResult> ChangePhoneNumber(User user, string phoneNumber, string token)
+        {
+            return await _userManager.ChangePhoneNumberAsync(user, phoneNumber, token);
+        }
+
+        public async Task<IdentityResult> RemovePassword(User user)
+        {
+            return await _userManager.RemovePasswordAsync(user);
+        }
+
+        public string HashPassword(User user, string password)
+        {
+            return _passwordHasher.HashPassword(user, password);
+        }
+
+        public async Task<IdentityResult> Update(User user)
+        {
+            if (user.FirstName == null)
+                throw new NullReferenceException(nameof(User.FirstName));
+
+            if (user.LastName == null)
+                throw new NullReferenceException(nameof(User.LastName));
+
+            user.LastEditDate = DateTime.Now;
+            user.EmailConfirmed = user.PhoneNumberConfirmed = true;
+
+            return await _userManager.UpdateAsync(user);
+        }
+
+        public async Task<User> FindById(string id)
+        {
+            return await _userManager.FindByIdAsync(id);
+        }
+
+        public async Task<IList<string>> GetRoles(User user)
+        {
+            return await _userManager.GetRolesAsync(user);
+        }
+
+        public async Task<IdentityResult> Insert(User user, string password)
+        {
+            if (user.FirstName == null)
+                throw new NullReferenceException(nameof(User.FirstName));
+
+            if (user.LastName == null)
+                throw new NullReferenceException(nameof(User.LastName));
+
+            user.CreateDate = user.LastEditDate = DateTime.Now;
+            user.EmailConfirmed = user.PhoneNumberConfirmed = true;
+
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public async Task<IdentityResult> AddToRole(User user, string role)
+        {
+            return await _userManager.AddToRoleAsync(user, role);
+        }
+
+        public async Task<IdentityResult> UpdateRole(User user, string newRole)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, roles);
+            if (result.Succeeded)
+            {
+                return await AddToRole(user, newRole);
+            }
+
+            return IdentityResult.Failed();
+        }
+
+        public async Task<bool> IsUserAllowedForOperation(User currentUser, string operationUserId, string alwaysAllowedUserRole)
+        {
+            return operationUserId == currentUser.Id || await IsInRole(currentUser, alwaysAllowedUserRole);
+        }
+
+        public async Task<int> Count()
+        {
+            return await _userManager.Users.CountAsync();
+        }
+
+        public async Task<User> FindByEmail(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<string> GeneratePasswordResetToken(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
+        public async Task<IdentityResult> ResetPassword(User user, string token, string newPassword)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, newPassword);
+        }
+    }
+}
