35eacdc481bd252ce028289de7b03b91345584de
Ali Momenzadeh <alimomenzadeh78@gmail.com>
2021-05-10 07:21:25 +0430

    Add ProgressBars viewComponent

---
+++ AMZEnterpriseWebsite/ViewComponents/ProgressBarsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using AMZEnterpriseWebsite.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AMZEnterpriseWebsite.ViewComponents
+{
+    public class ProgressBarsViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProgressBarsViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var progressBars = await _unitOfWork.ProgressBarRepository
+                .GetAll()
+                .OrderBy(x => x.SortIndex)
+                .ToListAsync();
+
+            return View(progressBars);
+        }
+    }
+}
